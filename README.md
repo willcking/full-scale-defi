d0cf8dab7872bb1bea3b9bf2278b00a348ce3c2a
willcking <19372142252@163.com>
2024-09-07 17:29:19 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # full-scale-defi
+
+## breif
+Full-scale-Defi is a comprehensive project that integrates coin issuance, staking, swapping, lending, cross-chain support, and KYC certification.
---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+
+  networks: {
+    local: {
+      url: 'http://127.0.0.1:8545',
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
