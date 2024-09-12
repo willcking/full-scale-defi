8ee3b40158efdb848ac3e8fe5af722ffd25a98a3
willcking <19372142252@163.com>
2024-09-12 23:15:55 +0800

    Initial commit

---
+++ contract/MaxStake.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "./interface/IERC20.sol";
+import "./token/ERC20.sol";
+import "./interface/IMaxStake.sol";
+import "./utils/ReentrancyGuard.sol";
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+
+contract MaxStake is IMaxStake, ReentrancyGuard {
+
+}
