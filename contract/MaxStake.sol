2ee9b73bf10332bd3fc11f1d581b0cfa39d4c9dc
willcking <19372142252@163.com>
2024-09-16 23:47:41 +0800

    Initial commit

--- contract/MaxStake.sol
+++ contract/MaxStake.sol
@@ -13,7 +13,7 @@ import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol"
 import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 
 contract MaxStake is IMaxStake,ReentrancyGuard,Initializable,UUPSUpgradeable,AccessControlUpgradeable,PausableUpgradeable {
-    IERC20 public rewardtoken;
+    IERC20 public rewardToken;
     IERC20 public interestToken;
 
     uint256 public initInterest;
@@ -24,8 +24,118 @@ contract MaxStake is IMaxStake,ReentrancyGuard,Initializable,UUPSUpgradeable,Acc
     uint256 public totalRewards;
     uint256 public paidout;
     address public _owner;
+
     Pool[] public pools;    
     // User deposits in the liquidity pool
     mapping(uint256 => mapping(address => User)) userInfo;
     
+    function initialize(address _b2stAddress, uint256 _rewardPerSecond, uint256 _startTimeStamp, uint256 _endTimeStamp) external initializer {
+        require(_startTimeStamp < _endTimeStamp, "Invalid time");
+        require(_endTimeStamp > block.timestamp, "Invalid end time");
+        rewardToken = IERC20(_b2stAddress);
+        rewardPerSecond = _rewardPerSecond;
+        startTimeStamp = _startTimeStamp;
+        endTimeStamp = _endTimeStamp;
+        _owner = msg.sender;
+
+        __UUPSUpgradeable_init();
+    }
+
+    //Update liquidity pool data and call when liquidity changes.
+    function updatePool(uint256 pid) internal {
+        Pool storage pool = pools[pid];
+
+        uint256 lastTime = blocks.timestamp < endTimeStamp ? block.timestamp : endTimeStamp;
+        if(lastTime <= pool.lastRewardBlock) {
+            return;
+        }
+
+        uint256 totalSupply = pool.stTokenAmount;
+        if(totalSupply == 0) {
+            pool.lastRewardBlock = block.timestamp;
+            return;
+        }
+
+        uint256 effectTime = lastTime - pool.lastRewardBlock;
+        uint256 accRewardPerST = pool.accRewardPerST;
+
+        uint256 reward = rewardPerSecond * (pool.poolWeight) * (effectTime) / (totalAllocPoint);
+        accRewardPerST = accRewardPerST + (reward * (1e36) / (totalSupply)); 
+
+        pool.accRewardPerST = accRewardPerST;
+        pool.lastRewardBlock = block.timestamp;
+
+        emit UpdatePool(pid, pool.lastRewardBlock, reward);
+    }
+
+    function Stake(uint256 pid, uint256 amount) external {
+        require(block.timestamp < endTimeStamp, "Staking has ended");
+        Pool storage pool = pools[pid];
+        require(amount >= pool.minDepositAmount, "Amount less than limit");
+
+        User storage user = userInfo[pid][msg.sender];
+
+        //Update data every time user stake
+        updatePool(pid);
+
+        //claim reward
+        if(user.stAmount > 0) {
+            uint256 reward = pending(pid, msg.sender);
+            user.finishedAmount += reward;
+            user.pendingAmount = 0;
+
+            rewardToken.transfer(msg.sender, reward);
+        } else {
+            user.pendingAmount = user.stAmount * (pool.accRewardPerST) / (1e36) - user.finishedAmount; 
+        }
+
+        IERC20(pool.stTokenAddress).transferFrom(msg.sender, address(this), amount);
+
+        user.stAmount += amount;
+        pool.stTokenAmount += amount;
+
+        emit Stake(pid, amount);
+    }
+
+    function Withdraw(uint256 pid, uint256 amount) external {
+        require(amount > 0, "Invalid Amount");
+        User storage user = userInfo[pid][msg.sender];
+        require(amount <= user.stAmount, "The balance less than amount");
+
+        Pool storage pool = pools[pid];
+
+        updatePool(pid);
+        
+        uint256 reward = pending(pid, msg.sender);
+        user.finishedAmount += reward;
+        user.pendingAmount = 0;
+        rewardToken.transfer(msg.sender, reward);
+
+        pool.stTokenAmount -= amount;
+        user.stAmount -= amount;
+
+        IERC20(pool.stTokenAddress).transfer(msg.sender, amount);
+
+        emit Withdraw(pid, amount);
+    }
+
+    function claimReward(uint256 pid) external {
+        User storage user = userInfo[pid][msg.sender];
+        
+        uint256 reward = pending(pid, msg.sender);
+        user.finishedAmount += reward;
+        user.pendingAmount = 0;
+        rewardToken.transfer(msg.sender, reward);
+
+        emit Reward(pid);
+    }
+
+    function pending(uint256 pid, address user) internal view returns(uint256) {
+        User storage user = userInfo[pid][user];
+        Pool storage pool = pools[pid];
+
+        uint256 accRewardPerST = pool.accRewardPerST;
+        uint256 totalSupply = pool.stTokenAmount;
+        if(totalSupply > 0 && block.timestamp > pool.lastRewardBlock && )
+    }
 }
--- contract/interface/IMaxStake.sol
+++ contract/interface/IMaxStake.sol
@@ -15,8 +15,7 @@ interface IMaxStake {
         address stTokenAddress;
         uint256 poolWeight;
         uint256 lastRewardBlock;
-        // 每个代币奖励质押B2份额
-        uint256 accB2PerST;
+        uint256 accRewardPerST;
         uint256 stTokenAmount;
         uint256 minDepositAmount;
         uint256 minUnstakeAmount;
@@ -26,20 +25,20 @@ interface IMaxStake {
         uint256 borrowingRewardAmount;
     }
 
-    event Deposit(uint256 _pid,uint256 amount);
-    event Withdraw(uint256 _pid,uint256 amount);
-    event Reward(uint256 _pid);
+    event Stake(uint256 pid,uint256 amount);
+    event Withdraw(uint256 pid,uint256 amount);
+    event Reward(uint256 pid);
     event WithdrawPaused();
     event ClaimPaused();
     event WithdrawUnPaused();
     event ClaimUnPaused();
-    event UpdatePool(uint256 idx, uint256 lastRewardBlock, uint256 reward);
-    event DepositLend(uint256 _pid,uint _amount);
-    event WithdrawLend(uint256 _pid,uint _amount);
-    event ClaimLend(uint256 _pid);
-    event DepositBorrow(uint256 _pid,uint _amount);
-    event WithdrawBorrow(uint256 _pid,uint _amount);
-    event ClaimBorrow(uint256 _pid);
-    event Redeem(uint256 _pid,uint256 borrowAmt,uint256 collateralReward,uint256 accumulateInterest,address receiver);
-    event Settle(uint256 _pid,uint256 landingAmount,uint256 landingRewardAmount,uint256 totalInterest,address receiver);
+    event UpdatePool(uint256 pid, uint256 lastRewardBlock, uint256 reward);
+    event DepositLend(uint256 pid,uint amount);
+    event WithdrawLend(uint256 pid,uint amount);
+    event ClaimLend(uint256 pid);
+    event DepositBorrow(uint256 pid,uint amount);
+    event WithdrawBorrow(uint256 pid,uint amount);
+    event ClaimBorrow(uint256 pid);
+    event Redeem(uint256 pid,uint256 borrowAmt,uint256 collateralReward,uint256 accumulateInterest,address receiver);
+    event Settle(uint256 pid,uint256 landingAmount,uint256 landingRewardAmount,uint256 totalInterest,address receiver);
 }
